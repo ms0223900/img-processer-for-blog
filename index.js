5586c32aa76e06cf715a19340c22067592679440
ms0223900 <ms0223900@gmail.com>
2022-08-21 01:29:04 +0800

    feat: optimize output results

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const {
   ImgurClient,
 } = require('imgur');
 const fs = require('fs')
+// const rmOut = require('./rmOut')
 
 const IMG_FOR_PROCESS_DIR = './img-for-process'
 const OUT_DIR = './out'
+const WRITTEN_FILE_PATH = './uploaded.txt'
 
 const client = new ImgurClient({
   clientId: process.env.CLIENT_ID,
@@ -16,11 +18,11 @@ const uploadToImgur = (imgPath = '') => client.upload({
   type: 'stream',
 });
 
-const removeProcessedFiles = () => {
-  fs.rmSync(IMG_FOR_PROCESS_DIR, {
+const removeProcessedFiles = (dir = IMG_FOR_PROCESS_DIR) => {
+  fs.rmSync(dir, {
     recursive: true,
   })
-  fs.mkdirSync(IMG_FOR_PROCESS_DIR)
+  fs.mkdirSync(dir)
 }
 
 const readFilesFromOutDir = () => {
@@ -28,23 +30,58 @@ const readFilesFromOutDir = () => {
   return fileList;
 }
 
+const makeTimeStampStrDevider = () => (
+  `-----------------------${new Date().toISOString()}-----------------------`
+)
+
+const makeMarkdownImgStr = (imgSrc = '', imgAlt = '') => (
+  `![${imgAlt}](${imgSrc})`
+)
+
+// https://stackoverflow.com/questions/3459476/how-to-append-to-a-file-in-node
+const writeUploadedLinksToFile = (uploadeLinkList = []) => {
+  const stream = fs.createWriteStream(WRITTEN_FILE_PATH, {
+    flags: 'a',
+  });
+  stream.write(
+    '\n' + [makeTimeStampStrDevider(), ...uploadeLinkList].join('\n') + '\n'
+  )
+}
+
 function main() {
   (async () => {
-    console.log('Remove processed files...');
+    const fileList = readFilesFromOutDir();
+    if (!fileList.length) return console.log(
+      `No image found, please put images to ${IMG_FOR_PROCESS_DIR} folder.`
+    )
+
+    console.log('Remove compressed files.');
     removeProcessedFiles();
 
     console.log('Start uploading...');
     const uploadeLinkList = [];
-    const fileList = readFilesFromOutDir()
 
     for (const filePath of fileList) {
       const res = await uploadToImgur(`${OUT_DIR}/${filePath}`);
       // console.log(res.data);
-      uploadeLinkList.push(res.data.link);
+      uploadeLinkList.push({
+        link: res.data.link,
+        path: filePath,
+      });
+      console.log(`File ${filePath} uploaded.`)
     }
+    console.log('---Upload completed---')
 
-    fs.writeFileSync('./uploaded.txt', uploadeLinkList.join('\n'))
-    console.log('Upload completed :)')
+    const strPathAndLinkList = uploadeLinkList.map(l => `${l.path}\n${l.link}`)
+    const strLinkList = uploadeLinkList.map(l => l.link)
+    const mdImgList = uploadeLinkList.map(l => (
+      makeMarkdownImgStr(l.link, l.path)
+    ))
+    writeUploadedLinksToFile(
+      [...strPathAndLinkList, '---', ...strLinkList, '---Markdown format---', ...mdImgList]
+    )
+    console.log('Clear uploaded images.')
+    // removeProcessedFiles(OUT_DIR)
   })();
 }
 
